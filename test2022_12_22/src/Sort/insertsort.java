76bed9a995c41cf3854a79dd50a03271cfe499d0
guobinglin <2064555556@qq.com>
2022-12-22 19:06:31 +0800

    2022.12.22

---
+++ test2022_12_22/src/Sort/insertsort.java
@@ -0,0 +1,27 @@
+package Sort;
+
+public class insertsort implements Sort{
+    /**
+     * 插入排序
+     * 时间复杂度O(N^2)
+     * 最优O(N)
+     * 空间复杂度O(1)
+     * 稳定性：稳定
+     * @param array
+     */
+    @Override
+    public void sort (int[] array) {
+        for (int i = 1 ; i < array.length ; i++) {
+            int tmp=array[i];
+            int j=i-1;
+            for (; j>=0;j--){
+                if(array[j]>tmp){
+                    array[j+1]=array[j];
+                }else {
+                    break;
+                }
+            }
+            array[j+1]=tmp;
+        }
+    }
+}
